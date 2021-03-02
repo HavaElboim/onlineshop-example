bcbaf9b8ffa6ccda339b6c25f855dd0363b63c4a
HavaElboim <h.elboim@gmail.com>
2021-03-02 08:49:21 +0200

    set up server for online shop

--- server.js
+++ server.js
@@ -6,6 +6,43 @@ const app = express();
 
 // serve the 'public' folder which contains the json data:
 app.use(express.static("public"));
+// then the products.json will be accessible directly from:
+// http://192.168.43.81:8000/products.json
+
+// !! The attempt to fetch data from this server was refused with:
+//
+// Access to fetch at 'http://localhost:8000/products.json' from origin
+// 'http://localhost:3000' has been blocked by CORS policy:
+// No 'Access-Control-Allow-Origin' header is present on the requested
+// resource. If an opaque response serves your needs, set the request's
+// mode to 'no-cors' to fetch the resource with CORS disabled.
+//
+// Found a solution to the problem here:
+// https://www.codegrepper.com/code-examples/javascript/express+cors+allow+all+origins
+// and here:
+// https://www.codegrepper.com/code-examples/javascript/cors+allow+all+origins+nodejs
+//
+// installing cors:
+// npm install -save cors
+//
+// & need to add:
+// var cors = require('cors')  //use this
+// app.use(cors());
+//
+// but see also here an explanation for why CORS blocks the fetch here:
+// https://flaviocopes.com/express-cors/
+//
+// The fetch is blocked if it comes from a different port than that which the
+// server sits on.
+// So if npm start accesses localhost on port 3000,
+// set the server to serve at port 3000 also.
+//
+// But then npm start gives the following error:
+// Something is already running on port 3000.
+// Would you like to run the app on another port instead?
+
+var cors = require("cors");
+app.use(cors());
 
 // provide data to be displayed on the / page of the server's website
 app.get("/", (req, res) => {
@@ -22,7 +59,8 @@ const PORT = 8000;
 
 //  listen for any requests that come in on port PORT (8000)
 app.listen(PORT, () => {
-  console.log(`server listening on port ${PORT}`);
+  //  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8000");
+  console.log(`CORS-enabled web server listening on port ${PORT}`);
 });
 
 // running ipconfig in terminal to find local IP address:
