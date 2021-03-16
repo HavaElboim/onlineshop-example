9d27be390113b734459b299e5557954ea47d4c1f
HavaElboim <h.elboim@gmail.com>
2021-03-16 04:38:46 +0200

    clear new prod form after add, add prod to cart

--- client/src/components/ShoppingCartDisplay/ShoppingCartDisplay.js
+++ client/src/components/ShoppingCartDisplay/ShoppingCartDisplay.js
@@ -1,28 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Link } from "react";
+import CartItem from "../CartItem/CartItem";
+import "../../components/storagetools/LocalStorageArrayTools.js";
 
 const ShoppingCartDisplay = () => {
-const shoppingCartItems = localStorage.getItem("shoppingCart");
+  const shoppingCartItems = localStorage.getItem("shoppingCart");
 
-    return (
-        <div>
-{shoppingCart.map((cartItem) => (
-            <Link
-              className="cart-item"
-              to={`/shoppingCart/${cartItem.id}`}
-              key={cartItem.id}
-            >
-              <CartItem
-                itemName={cartItem.name}
-                itemImage={cartItem.image}
-                origPrice={cartItem.price}
-                sale={cartItem.isSale}
-                finalPrice={cartItem.finalPrice}
-                quantity={cartItem.quantity}
-              ></CartItem>
-            </Link>
-        </div>
-    );
-}
+  return (
+    <div>
+      {shoppingCartItems.map((cartItem) => (
+        <Link
+          className="cart-item"
+          to={`/shoppingCart/${cartItem.id}`}
+          key={cartItem.id}
+        >
+          <CartItem
+            itemName={cartItem.name}
+            itemImage={cartItem.image}
+            origPrice={cartItem.price}
+            sale={cartItem.isSale}
+            finalPrice={cartItem.finalPrice}
+            quantity={cartItem.quantity}
+          ></CartItem>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 ShoppingCartDisplay.propTypes = {
   /*  secondsLeft: PropTypes.number,
@@ -33,6 +36,6 @@ ShoppingCartDisplay.propTypes = {
     image: PropTypes.string,
     salesProductsIds: PropTypes.arrayOf(PropTypes.number),
     category: PropTypes.string,*/
-  };
-  
-  export default ShoppingCartDisplay;
+};
+
+export default ShoppingCartDisplay;
