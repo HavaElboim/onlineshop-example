866d852657e279a35d9fee0f5a8399148e9b6292
Hava Elboim <h.elboim@gmail.com>
2021-06-14 14:46:56 +0300

    set up user states for real login

--- client/src/contexts/UserContexts.js
+++ client/src/contexts/UserContexts.js
@@ -1,4 +1,7 @@
-import { createContext } from "react";
+// import { createContext } from "react";
+import { createContext, useCallback, useContext, useReducer } from "react";
+import { initialState, reducer } from "./ReducerContexts.js";
+
 
 const users = {
   guest: {
@@ -13,7 +16,22 @@ const users = {
 
 let currentUser = users.guest;
 
-const UserContext = createContext(null);
+// const UserContext = createContext(null);
+const UserContext = createContext(initialState);
+export function UserStateProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <UserContext.Provider value={{ dispatch, state }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+export function useUserState() {
+  return useContext(UserContext).state;
+}
+export function useUserDispatch() {
+  return useContext(UserContext).dispatch;
+}
 
-export { users, currentUser };
-export default UserContext;
+export { users, currentUser, UserContext };
+// export default UserContext;
