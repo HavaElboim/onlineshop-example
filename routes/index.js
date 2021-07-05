ccbe5884a84eee93b8667d27be2a40e0d7e777a3
Hava Elboim <h.elboim@gmail.com>
2021-07-05 07:50:30 +0300

    added authorization routes to server

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,14 @@
-import userRoutes from './user';
+  /* When you have 'type: module' in the package.json file, your source code should use import syntax. When you do not have, you should use require syntax. */
+// import userRoutes from './user';
+const userRoutes = require('./user');
+
 
 // syntactic sugar for { userRoutes: userRoutes }
-export { userRoutes };
+/* for use of import and export syntax in backend, see:
+https://stackoverflow.com/questions/38296667/getting-unexpected-token-export
+& also:
+https://stackoverflow.com/questions/31354559/using-node-js-require-vs-es6-import-export
+*/
+
+// export { userRoutes };
+module.exports = userRoutes;
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,26 @@
-import Joi from 'joi'; /* for input validation */
-import express from 'express';
-import User from '../models/user';
-/* import signUp validator: */
-import { signUp } from '../validations/user';
 
-const userRouter = express.Router();
+// import Joi from 'joi'; /* for input validation */
+// 'import' is ES6 notation, not supported here in the backend.
+// using instead the CommonJS format - 'require':
+// const joi = require('joi'); /* for input validation */
+// import express from 'express';
+const express = require("express");
+// import User from '../models/user';
+const User = require('../models/user');
+
+const bcrypt = require('bcrypt');
+// const jwt = require('jwt');
+const jwt = require('jsonwebtoken');
+const salt = 10;
+const secret = process.env.TOKEN_SECRET;
+
+
+/* import signUp validator: */
+// import { signUp } from '../validations/user.mjs';
+// const signUp = require('../validations/user');
+const router = express.Router();
 /* we used an empty string for our path in routes/user.js—because we provide the “/users” path in index.js: */
-userRouter.post("", async (req, res) => {
+router.post('add-user', async (req, res) => {
     /*
         modify our post method to use async and a try/catch block:
     */
@@ -15,61 +29,52 @@ try {
     /* destructure the req.body, 
      and call the Joi.validate method passing in an object 
      containing input from the user and our signUp validator:*/
-    const { username, email, password } = req.body
-    await Joi.validate({ username, email, password }, signUp);
-    const newUser = new User({ username, email, password });
+    const { user:userReq } = req.body;
+    const {password, ...userDetails} = userReq;
+    const user = new User({userDetails, hash: await bcrypt.hash(password, salt)});
+    const savedUser = await user.save();
+    res.json(savedUser);
+    // await Joi.validate({ email, password }, allowUnknown, signUp);
+    // const newUser = new User({ email, password });
     /*
         If that passes, we define a new User instance 
         and attempt to save that user:
     */
-    await newUser.save();
+    // await newUser.save();
     /*
         And ff that also passes, we send back exactly what we want 
         our frontend to have access to:
     */
-    res.send({ userId: newUser.id, username });
+    // res.send({ userId: newUser.id, email });
   } catch (err) {
       /*
         If anything fails, it will be caught and we respond by setting the status code to 400 
         and sending the error back:
       */
-    res.status(400).send(err);
+    // res.status(400).send(err);
+    res.send(err.message);
   }
 });
-export default userRouter;
 
-/* add a “pre-hook” or “middleware” for our UserSchema. 
-This code will run right before our User instance saves. 
-
-“this” is our pre method refers to the User instance. 
-We double check that a password has been provided, 
-then reset that password to its salted and hashed version.
-*/
-UserSchema.pre('save', function () {
-    if (this.isModified('password')) {
-      this.password = hashSync(this.password, 10);
+router.post('/login', async (req, res) => {
+    const {user:userReq} = req.body;
+    const user = await User.findOne({email: userReq.email});
+    try{
+        const {hash, userSign} = user;
+        const match = await bcrypt.compare(userReq.password, user.hash);
+        const accessToken = jwt.sign(userSign, process.env.TOKEN_SECRET, {
+            expiresIn: "30000s"
+        })
+        if(match){
+            res.json({accessToken: accessToken});
+        } else {
+            res.json({message: "Invalid Credentials"});
+        }
+    } catch(e) {
+        console.log(e);
     }
-  });
+});
 
-  /* 
-  our custom validator, doesNotExist, which is a class method defined on our UserSchema. 
-  Using two mongoose methods and a comparison operator we can determine if a user exists or not. 
-  This is an asynchronous step, so we use async/await.
-  */
-  UserSchema.statics.doesNotExist = async function (field) {
-    return await this.where(field).countDocuments() === 0;
-  };
+// export default userRoutes;
+module.exports = router;
 
-  /*
-We also define an instance method, comparePasswords, which simply compares a 
-plain text password with a User instance’s hashed password.
-  */
-  UserSchema.methods.comparePasswords = function (password) {
-    return compareSync(password, this.password);
-  };
-
-  /*
-Lastly, we explicitly define our User model and export:
-  */
-  const User = mongoose.model('User', UserSchema);
-  export default User;
