f7e6dcb42d59e98433f845e499f1b9e2049c218b
Hava Elboim <h.elboim@gmail.com>
2021-07-05 11:35:03 +0300

    Tidied up and put back missing code

--- client/src/pages/ProductInfo/ProductInfoAdmin.js
+++ client/src/pages/ProductInfo/ProductInfoAdmin.js
@@ -9,52 +9,29 @@ import { React, useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
 import "./ProductInfo.css";
-import ThemeContext, { themes } from "../../contexts/ThemeContexts";
-import SaleContext, { sales } from "../../contexts/SaleContexts";
+import ThemeContext from "../../contexts/ThemeContexts";
+import SaleContext from "../../contexts/SaleContexts";
 
 import "../../components/storagetools/LocalStorageArrayTools.js";
 import CategorySelectAdmin from "../../components/CategorySelectAdmin/CategorySelectAdmin";
-// import saleIcon from "../../components/icons/sale-icon-png-19.png";
-//import saleIcon from "../../components/icons/saleGreenBig.png";
+
 import saleIcon from "../../components/icons/green-leaf-sale-icon.png";
 import LeavesFrame from "../../components/icons/green-leaves-left-frame-1.svg";
-import CartIcon from "../../components/CartIcon/CartIcon";
-
-// custom hook for updating state from local storage
-import createPersistedState from "use-persisted-state";
-const useCartState = createPersistedState("cart");
-/*
-mongodb+srv://test-user1:12345@cluster0.u00wy.mongodb.net/gocodeshop-hava?retryWrites=true&w=majority&tlsInsecure=true
-*/
-
-/* mongoDB version on localhost:
-useEffect(() => {
-    fetch(`http://10.0.0.193:8000/products/${match.params.productid}`)
-      .then((response) => response.json())
-      .then((data) => setData(data));
-    console.log(
-      "descr is ",
-      productData,
-      ` from http://10.0.0.193:8000/products/${match.params.productid}`
-    );
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  */
 
 const ProductInfoAdmin = ({ match }) => {
   const [products, setProducts] = useState({});
 
   const { theme } = useContext(ThemeContext);
   const { sale } = useContext(SaleContext);
-  const [quantity, setQuantity] = useState(1);
+  
   const [editProduct, setEditProduct] = useState(false);
-  const [notAllFieldsFilled, setFieldsFilled] = useState(false);
+  const [notAllFieldsFilled] = useState(false);
 
   const [selectedCategory, setSelectedCategory] = useState("");
   const [onSale, setSale] = useState("false");
   const [saleReductionPercent, setReduction] = useState("");
 
-  const [productId, setId] = useState("");
+  const [ setId] = useState("");
   const [productName, setName] = useState("");
   const [productDescription, setDescription] = useState("");
   const [productURL, setURL] = useState("");
@@ -62,24 +39,16 @@ const ProductInfoAdmin = ({ match }) => {
   const [quantityInStock, setStockQuantity] = useState("");
 
   const [stopEditText, setStopEditText] = useState("Exit product update");
-  const [quantityWarnText, setQtyWarn] = useState("");
 
   const deleteProductText = "Delete product";
-  console.log("in productinfo, theme is: ", theme.foreground);
-  //console.log("sale is", sale.isSale);
-  const [cart, setCart] = useCartState({});
-  const [numInCart, setNumInCart] = useState(cart.length>0 ? cart.reduce((n, { quantity }) => n + quantity, 0): 0);
 
-  console.log("!!!!!!!!!!!!!!!!!!!!! In ProductInfo, numInCart is: ", numInCart);
   useEffect(() => {
     fetch(`/api/products/${match.params._id}`)
       .then((response) => response.json())
       .then((data) => setProducts(data));
     console.log("descr is ", products, ` /api/products/${match.params._id}`);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  //const newPrice = +(productPrice * 0.9).toFixed(2);
 
   const deleteProductFn = async (id) => {
     console.log(`deleting product ${id}`);
@@ -115,7 +84,7 @@ const ProductInfoAdmin = ({ match }) => {
     setName(title);
     setSelectedCategory(category);
     setSale(onSale);
-
+    setURL(url); /* I put this back in today 5/7 */
     setEditProduct(false);
     setStopEditText("Done");
     setReduction(saleReductionPercent);
@@ -151,22 +120,16 @@ const ProductInfoAdmin = ({ match }) => {
     setSale(products.onSale);
     setReduction(products.saleReductionPercent);
     setId(match.params._id);
+    setURL(products.url); /* I put this back in today 5/7 */
   };
-
-  
-
-  // see here for how to add objects to localStorage:
-  // https://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage/23516713#23516713
-  // https://stackoverflow.com/questions/62504525/persistence-with-localstorage-with-usestate-and-usecontext-react-hooks
-  // https://stackoverflow.com/questions/63925254/useeffect-localstorage-loop
-  // https://egghead.io/lessons/react-store-values-in-localstorage-with-the-react-useeffect-hook
-
-  
   
   if (products) {
     return (
            <div className="outer-group">       
-            {editProduct ? (
+            {editProduct ? 
+            
+            
+            (
               <div>
                 <div>
                   <button
@@ -202,6 +165,50 @@ const ProductInfoAdmin = ({ match }) => {
                {productName.length === 0 && notAllFieldsFilled && (
                  <label for="productName">Enter name of product</label>
                )}
+               
+               <div className="displayImageOuterBox">
+                  <div className="displayImageInnerBox">
+                    {products.onSale ? (
+                      <div className="saleIconImg">
+                        <img  src={saleIcon} alt="on sale"className="product-image" />
+                  </div>
+                ): (
+                  <div className="saleIconImg">
+                  <img  src={LeavesFrame} alt="not on sale" className="product-image"/>
+                </div>
+                )}
+                 <img src={products.image} alt={""} className="product-image"/>
+                 </div>
+              </div>
+     
+                <div>
+            <div>
+              <div>Input description of product:</div>
+              <input
+                id="productDescription"
+                value={productDescription}
+                onChange={(e) => setDescription(e.target.value)}
+                style={{
+                  color: theme.background,
+                  background: theme.foreground,
+                }}
+              />{" "}
+            </div>
+             <div>
+             <div>Input price of product:</div>
+             <input
+               id="productPrice"
+               value={productPrice}
+               onChange={(e) => setPrice(e.target.value)}
+               style={{
+                 color: theme.background,
+                 background: theme.foreground,
+               }}
+             />
+             {products.price.length === 0 && notAllFieldsFilled && (
+               <label for="productPrice">Enter price of product</label>
+             )}
+           </div>      
               <div>
                 {products.length > 0 && (
                   <CategorySelectAdmin
@@ -274,35 +281,12 @@ const ProductInfoAdmin = ({ match }) => {
                   </label>
                 )}
             </div>
-            <div>
-              <div>Input description of product:</div>
-              <input
-                id="productDescription"
-                value={productDescription}
-                onChange={(e) => setDescription(e.target.value)}
-                style={{
-                  color: theme.background,
-                  background: theme.foreground,
-                }}
-              />{" "}
-            </div>
-            <div>
-              <div>Input price of product:</div>
-              <input
-                id="productPrice"
-                value={productPrice}
-                onChange={(e) => setPrice(e.target.value)}
-                style={{
-                  color: theme.background,
-                  background: theme.foreground,
-                }}
-              />
-              {products.price.length === 0 && notAllFieldsFilled && (
-                <label for="productPrice">Enter price of product</label>
-              )}
-            </div>
           </div>
+          </div>
+
         ) : (
+
+
           <div>
               <div>
                 <button
@@ -356,7 +340,11 @@ const ProductInfoAdmin = ({ match }) => {
                       ).toFixed(2)}
                   </div>
                 </div>
+
+
             ) : (
+
+
               <div className="product-info">
                 $ {productPrice || products.price}
               </div>
@@ -367,8 +355,13 @@ const ProductInfoAdmin = ({ match }) => {
           <div className="product-info">
             Number of product in stock: {products.quantityInStock}{" "}
           </div>
+          <div className="product-info">
+                {productDescription || products.description}
+          </div>
         </div>
       </div>
+
+
     )}
     </div>
     );
